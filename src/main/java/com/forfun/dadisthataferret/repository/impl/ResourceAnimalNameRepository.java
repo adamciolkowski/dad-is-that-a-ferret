ddbdb4ccd7df5e1eceb8e218fc8f9e24abf715d8
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-10-02 13:29:39 +0200

    Use existing JDK method

--- src/main/java/com/forfun/dadisthataferret/repository/impl/ResourceAnimalNameRepository.java
+++ src/main/java/com/forfun/dadisthataferret/repository/impl/ResourceAnimalNameRepository.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.forfun.dadisthataferret.utils.Utils.randomElement;
+import static java.nio.file.Files.readAllLines;
 
 @Service
 public class ResourceAnimalNameRepository implements AnimalNameRepository {
@@ -24,22 +22,7 @@ public class ResourceAnimalNameRepository implements AnimalNameRepository {
     }
 
     public static List<String> loadAnimalNamesFrom(Resource resource) throws IOException {
-        try(BufferedReader reader = readerFrom(resource)) {
-            return linesToList(reader);
-        }
-    }
-
-    private static BufferedReader readerFrom(Resource resource) throws IOException {
-        return new BufferedReader(new InputStreamReader(resource.getInputStream()));
-    }
-
-    private static List<String> linesToList(BufferedReader reader) throws IOException {
-        List<String> animals = new ArrayList<>();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            animals.add(line);
-        }
-        return animals;
+        return readAllLines(resource.getFile().toPath());
     }
 
     @Override
